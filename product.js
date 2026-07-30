8504d36c94102ddfde1e0defd989f90e6ed817b8
dxofficials000 <aliezedavid000@gmail.com>
2026-07-30 08:49:51 +0100

    Add files via upload

---
+++ product.js
@@ -0,0 +1,59 @@
+const branditProducts = [
+    {
+        id: 1,
+        name: "Brandit Signature Wear",
+        image: "hero2.jpg"
+    },
+    {
+        id: 2,
+        name: "Custom Streetwear Piece",
+        image: "hero3.jpg"
+    },
+    {
+        id: 3,
+        name: "Heavyweight Blank Collection",
+        image: "hero6.jpg"
+    },
+    {
+        id: 4,
+        name: "Branded Headwear",
+        image: "hero9.jpg"
+    },
+    {
+        id: 5,
+        name: "Exclusive Drop Design",
+        image: "hero11.jpg"
+    },
+    {
+    id: 6,
+    name: "lounge wear",
+    image: "hero20.jpg"
+},
+{
+    id: 7,
+    name: "jorts",
+    image: "hero17.jpg"
+},
+{
+    id: 8,
+    name: "jean cap",
+    image: "hero23.jpg"
+},
+{
+    id: 9,
+    name: "jean",
+    image: "hero28.jpg"
+},
+{
+    id: 10,
+    name: "shirt",
+    image: "hero29.jpg"
+},
+{
+    id: 11,
+    name: "jean",
+    image: "hero25.jpg"
+}
+
+
+];
